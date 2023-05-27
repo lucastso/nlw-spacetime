58b4a2fb07458afe424feea1011297fe58f99f3b
Lucas Tassi Souza <lucastassisouza@gmail.com>
2023-05-27 18:41:53 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,35 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
+import { authRouter } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
+
+const app = fastify()
+const PORT = 3333
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: 'uploads',
+})
+app.register(cors, {
+  origin: true,
+})
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(multipart)
+app.register(memoriesRoutes)
+app.register(uploadRoutes)
+app.register(authRouter)
+
+app
+  .listen({
+    port: PORT,
+  })
+  .then(() => {
+    console.log(`Application: http://localhost:${PORT}`)
+  })
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Copyright = () => {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        href="#"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
+
+export default Copyright
